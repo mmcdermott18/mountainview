f0c988e8a820baf550e5355f03611f8e801ca625
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2024-09-03 15:41:19 -0400

    home page initial push

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.images').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+    });
+});
