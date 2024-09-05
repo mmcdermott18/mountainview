22f3bd4cfcae46f0be351165a0464d210130ffee
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2024-09-04 22:04:09 -0400

    done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    // run function on initial page load
+    gallery();
+    nav();
+    
+    // run function on resize of the window
+    $(window).resize(function() {
+
+    })
+    // run function on scroll
+    $(window).scroll(function() {
+
+    })
+});
+function gallery() {
   $('.images').slick({
     infinite: true,
     slidesToShow: 3,
@@ -9,4 +23,18 @@ $(document).ready(function(){
     autoplay: true,
     autoplaySpeed: 5000
     });
-});
+}
+function nav() {
+  $('#open').click(function(){
+    $('#open').addClass('hidden');
+    $('#close').removeClass('hidden');
+    $('#mobile-nav').removeClass('hidden');
+    $('body').addClass('scroll-lock');
+    $('#close').click(function(){
+      $('#open').removeClass('hidden');
+      $('#close').addClass('hidden');
+      $('#mobile-nav').addClass('hidden');
+      $('body').removeClass('scroll-lock');
+    });
+  });
+}
